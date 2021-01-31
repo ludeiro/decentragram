1a37b277c3ba989b3f228ff49229b882c1a7f948
ludeiro <arludeiro@gmail.com>
2021-01-31 19:25:16 +0100

    Basics

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,26 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  
+ 	string public name = "Decentragram";
+
+ 	//Store Images
+ 	mapping(uint => Image) public images;
+
+ 	struct Image {
+ 		uint id;
+ 		string hash;
+ 		string description;
+ 		uint tipAmount;
+ 		address payable author;
+ 	}
+
+ 	//Create Images
+ 	function uploadImage() public{
+ 		images[1] = Image(1, 'dawda', 'Hello World', 0, address(0x0));
+ 	}
+
+
+ 	// Tip Images
+
 }
