068849a9fd2e7338d4abc318328bc2d493d02ed5
ludeiro <arludeiro@gmail.com>
2021-02-01 23:38:04 +0100

    Conection to Ganache using web3js

--- src/components/App.js
+++ src/components/App.js
@@ -9,17 +9,52 @@ import Main from './Main'
 
 class App extends Component {
 
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    let accounts = await web3.eth.getAccounts()
+    this.setState( { account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+
+    if (networkData) {
+      const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+    }else{
+      window.alert("Decentragram contract is not deployed in the detected network");
+    }
+    
+  }
+
   constructor(props) {
     super(props)
     this.state = {
-      account: '',
+      name: 'Decentragram',
+      account: this.props.account,
     }
   }
 
   render() {
     return (
       <div>
-        <Navbar account={this.state.account} />
+        <Navbar account={this.state.account} name={this.state.name} />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
--- test/test.js
+++ test/test.js
@@ -96,6 +96,7 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       // FAILURE: Tries to tip a image that does not exist
       await decentragram.tipImageOwner(99, { from: tipper, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
     })
+     
 
   })
 
